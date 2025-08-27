dd7edca05e3b929cacccd847a0903d7811ed3205
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-08-27 08:21:54 -0500

    tercer proyecto

---
+++ semana 3/Proyectos basicos/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //operaciones
+    int val1 = 20;
+    float val2 = 8;
+    int suma = val1 + val2;
+    int resta = val1 - val2;
+    int mult = val1 * val2;
+    float div = val1 / val2;
+    cout << "La suma es:" << suma << endl;
+    cout << "La resta es:" << resta << endl;
+    cout << "La mult. es:" << mult << endl;
+    cout << "La div. es:" << div << endl;
+
+    return 0;
+}
